a34ecb2dddf1bc58138be7b32dd1cabd8d8fda82
Jessica Albert <jessmalbert0807@gmail.com>
2019-08-20 17:39:30 -0500

    Declare variable to hidden API key,

--- scripts.js
+++ scripts.js
@@ -1,14 +1,15 @@
-var request = new XMLHttpRequest()
+var request = new XMLHttpRequest();
 
-var milwaukee = 5263045
+var milwaukee = 5263045;
+var apikey = config.OPEN_WEATHER_KEY;
 
-request.open('GET', 'api.openweathermap.org/data/2.5/weather?id' + milwaukee, true)
+request.open('GET', 'api.openweathermap.org/data/2.5/weather?id=' + milwaukee + 'APPID=' + apikey, true);
 request.onload = function() {
-    var data = JSON.parse(this.response)
+    var data = JSON.parse(this.response);
     if(request.status >= 200 && request.status < 400){
-        console.log(weather.main)
+        console.log(weather.main);
     }
     else
-        console.log('error')
+        console.log('error');
 }
-request.send()
+request.send();
