bc40082070d9db49fa12d9c7dda9477b18ec1981
Jessica Albert <jessmalbert0807@gmail.com>
2019-08-22 21:27:11 -0500

    Added displayConditions function

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@
  * https://openweathermap.org/current
  * Arguments: cityName - string for the name of the city
  * Output: cityID for cityName, per city.list.json from Open Weather
- */
+ ****************************************************************************/
 function getCityID(cityName){
     if(cityName == "Milwaukee")
         return 5263045;
@@ -26,37 +26,38 @@ function getData(city){
     //var selection = document.getElementById("city").title;
     var selection = city.title;
     var cityID = getCityID(selection);
-    var weatherMain;
+    var weatherMain, temperatureF, humidity;
 
-    request.open("GET", "http://api.openweathermap.org/data/2.5/weather?id=" + cityID + "&APPID=" + apikey, true);
+    //Used this tutorial to learn how to make the API call https://www.taniarascia.com/how-to-connect-to-an-api-with-javascript/
+    request.open("GET", "http://api.openweathermap.org/data/2.5/weather?id=" + cityID + "&APPID=" + apikey + "&units=imperial", true);
     request.onload = function() {
         var data = JSON.parse(this.response);
         if(request.status >= 200 && request.status < 400){
-            console.log(data.weather[0].main);
-            //console.log(data.coord.lon);
-            document.getElementById("weather-title").innerHTML = "The Current Weather in " + selection;
-            weatherMain = "Clear";//data.weather[0].main;
+            weatherMain = data.weather[0].main;
+            temperatureF = data.main.temp;
             displayImage(selection, weatherMain);
+            displayConditions(selection, temperatureF, weatherMain);
             data = "";
         }
         else
-            //console.log('error');
             document.getElementById("printCity").innerHTML = "error";
     }
     request.send();
 }
 
 function displayImage(city, currentWeather){
-    //Realized that if another city is clicked that images continue to append. Called on Stack Overflow to help me resolve
+    //Realized that if another city is clicked without refreshing the page that images continue to append. Called 
+    //on Stack Overflow to help me resolve this issue
     //Check if the weather-img element has any children, remove the children if this is true
     //https://stackoverflow.com/questions/6802683/js-how-to-remove-image-with-javascript
     //https://stackoverflow.com/questions/2161634/how-to-check-if-element-has-any-children-in-javascript
     var image_x = document.getElementById("weather-img");
-    console.log(image_x.children.length)
     if(image_x.children.length > 0){
-        image_x.removeChild(image_x.childNodes[1]);
+        image_x.removeChild(image_x.childNodes[0]);
     }
     var img = document.createElement("img");
+
+    //If data set were to get much larger it would make sense to refactor this into a hash table
     if(city == "Milwaukee"){
         if(currentWeather == "Clear"){
             img.src = "https://uwm.edu/wp-content/uploads/2018/12/20181017_IMC_EH_0001_artmuseum.jpg";
@@ -71,7 +72,7 @@ function displayImage(city, currentWeather){
             img.src = "https://onmilwaukee.com/images/articles/up/uprainhailsleetsnow/uprainhailsleetsnow_fullsize_story1.jpg?1326830728";
         }
     }
-    if(city == "Minneapolis"){
+    else if(city == "Minneapolis"){
         if(currentWeather == "Clear"){
             img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Minneapolis_skyline_from_Prospect_Park_Water_Tower%2C_July_2014.jpg/900px-Minneapolis_skyline_from_Prospect_Park_Water_Tower%2C_July_2014.jpg";
         }
@@ -84,11 +85,46 @@ function displayImage(city, currentWeather){
         if(currentWeather == "Snow"){
             img.src = "https://s3-eu-west-1.amazonaws.com/acitymadebypeople/images/Articles/citinerary-minneapolis-wintersports-01.jpg?mtime=20170127164007";
         }
-
+    }
+    else if(city == "Chicago"){
+        if(currentWeather == "Clear"){
+            img.src = "https://emptyseats.files.wordpress.com/2012/09/skyline1.jpg?w=950&h=633";
+        }
+        if(currentWeather == "Clouds"){
+            img.src = "https://live.staticflickr.com/2475/3866435526_affac5ba04_b.jpg";
+        }
+        if(currentWeather == "Rain"){
+            img.src = "http://d1b8n6kng8vcnt.cloudfront.net/blog/wp-content/uploads/2013/11/cloudgate.jpg";
+        }
+        if(currentWeather == "Snow"){
+            img.src = "https://cdn.vox-cdn.com/thumbor/FcUeaOtIoWpp8ntAjhsv9m6X2fw=/0x0:1200x630/1200x800/filters:focal(504x219:696x411)/cdn.vox-cdn.com/uploads/chorus_image/image/63491243/weather_cst_010713_005_e1547666837623.0.jpg";
+        }
+    }
+    else if(city == "Dallas"){
+        if(currentWeather == "Clear"){
+            img.src = "https://images.unsplash.com/photo-1545194445-dddb8f4487c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80";
+        }
+        if(currentWeather == "Clouds"){
+            img.src = "https://i.redd.it/iwtzyai04aj01.jpg";
+        }
+        if(currentWeather == "Rain"){
+            img.src = "https://dallasnews.imgix.net/1537636445-2013__NM_9RAIND_29038760.JPG?auto=format%2Cenhance&crop=faces%2Centropy&fit=crop&q=40&or=0&w=412&h=549";
+        }
+        if(currentWeather == "Snow"){
+            img.src = "https://dfw.cbslocal.com/wp-content/uploads/sites/15909545/2011/02/108822685.jpg";
+        }
+    }
+    else{
+        img.src = ' ';
     }
     img.alt = "current weather image"
     img.height = "500";
-    img.width = "500";
+    img.width = "700";
     document.getElementById("weather-img").appendChild(img);
     
 }
+
+function displayConditions(city, temp, condition){
+    document.getElementById("weather-title").innerHTML = "The current weather in " + city + " is " + temp + "F and " + condition;
+}
+
